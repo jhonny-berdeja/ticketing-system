729d035e991319208fe72f918b2784056d156456
jhonny-berdeja <jhonny.berdeja.100@gmail.com>
2025-02-01 06:54:41 -0300

    moved componentMap to CustomForm whith new name availableComponents

--- src/form/CustomForm.jsx
+++ src/form/CustomForm.jsx
@@ -1,51 +1,68 @@
 import React, { useState } from "react";
 import "../styles/custom_button.css";
+import CustomInput from "../form/elements/CustomInput";
+import CustomInputAutocomplete from "../form/elements/CustomInputAutocomplete";
+import CustomSelectDynamic from "../form/elements/CustomSelectDynamic";
+import CustomSelectMultiple from "../form/elements/CustomSelectMultiple";
+import CustomCheckbox from "../form/elements/CustomCheckbox";
+import CustomTextArea from "../form/elements/CustomTextArea";
 
-const CustomForm = ({ fieldsData, componentMap, requestMethods, onSubmit }) => {
+// Los componentes disponibles
+const availableComponents = {
+  CustomInput: CustomInput,
+  CustomInputAutocomplete: CustomInputAutocomplete,
+  CustomSelectDynamic: CustomSelectDynamic,
+  CustomSelectMultiple: CustomSelectMultiple,
+  CustomCheckbox: CustomCheckbox,
+  CustomTextArea: CustomTextArea,
+};
+
+const CustomForm = ({ fieldsData, requestMethods, onSubmit }) => {
   const [formData, setFormData] = useState({});
 
-  // Función para manejar los cambios en los campos
   const handleChange = (fieldName, value) => {
     setFormData((prevData) => ({
       ...prevData,
-      [fieldName]: value && value.value ? value.value : value, // Asegúrate de extraer el valor si es necesario
+      [fieldName]: value && value.value ? value.value : value,
     }));
   };
 
-  // Función para manejar el envío del formulario
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    // Recorre los campos y asigna solo los valores a cada campo
     const formSubmissionData = fieldsData.map((field) => ({
       fieldData: field.fieldData,
-      value: formData[field.fieldData.name] || "", // Incluye el valor cargado o seleccionado
+      value: formData[field.fieldData.name] || "",
       component: field.component,
     }));
-
-    // Llamar a la función onSubmit pasada por el padre
     onSubmit(formSubmissionData);
   };
+
+  // Construir el componentMap dinámicamente en base a fieldsData
+  const componentMap = fieldsData.reduce((acc, field) => {
+    if (availableComponents[field.component]) {
+      acc[field.component] = availableComponents[field.component];
+    }
+    return acc;
+  }, {});
 
   return (
     <form onSubmit={handleSubmit}>
       {fieldsData.map((field, index) => {
-        const Component = componentMap[field.component]; // Se usa `component`, no `type`
+        const Component = componentMap[field.component];
 
-        if (!Component) return null; // Si el componente no existe, se omite
+        if (!Component) return null;
 
-        const methods = requestMethods[field.fieldData.name] || {}; // Obtiene métodos por `name`
+        const methods = requestMethods[field.fieldData.name] || {};
 
         return (
           <Component
             key={index}
             fieldData={field.fieldData}
             onRequest={methods}
-            onChange={(data) => handleChange(field.fieldData.name, data)} // Solo pasa el valor adecuado
+            onChange={(data) => handleChange(field.fieldData.name, data)}
           />
         );
       })}
-
       <button className="btn btn-primary btn-block mb-3" type="submit">
         Enviar
       </button>
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,141 +1,135 @@
 import React, { useState } from "react";
 import CustomForm from "../form/CustomForm";
-import CustomInput from "../form/elements/CustomInput";
-import CustomInputAutocomplete from "../form/elements/CustomInputAutocomplete";
-import CustomSelectDynamic from "../form/elements/CustomSelectDynamic";
-import CustomSelectMultiple from "../form/elements/CustomSelectMultiple"; // Nuevo import
-import CustomCheckbox from "../form/elements/CustomCheckbox";
-import CustomTextArea from "../form/elements/CustomTextArea"; // Import añadido
 import fieldsData from "../json/fields.json";
 
-const componentMap = {
-  CustomInput: CustomInput,
-  CustomInputAutocomplete: CustomInputAutocomplete,
-  CustomSelectDynamic: CustomSelectDynamic,
-  CustomSelectMultiple: CustomSelectMultiple, // Añadido CustomSelectMultiple
-  CustomCheckbox: CustomCheckbox,
-  CustomTextArea: CustomTextArea, // Añadido CustomTextArea
+const Home = () => {
+  const [formData, setFormData] = useState(null);
+  
+  // Métodos para 'userSearch'
+const userSearchMethods = {
+  searchUser: async (query) => {
+    const users = [
+      { value: "Aaron Mendoza", label: "Aaron Mendoza" },
+      { value: "Abel Rodríguez", label: "Abel Rodríguez" },
+      { value: "Abigail Fernández", label: "Abigail Fernández" },
+      { value: "Abril Gómez", label: "Abril Gómez" },
+      { value: "Adriana Pérez", label: "Adriana Pérez" },
+      { value: "Agustín Torres", label: "Agustín Torres" },
+      { value: "Aída Ramírez", label: "Aída Ramírez" },
+      { value: "Alan Domínguez", label: "Alan Domínguez" },
+      { value: "Ana Fernández", label: "Ana Fernández" },
+      { value: "Luis Rodríguez", label: "Luis Rodríguez" },
+      { value: "Sofía Ramírez", label: "Sofía Ramírez" },
+      { value: "Pedro Sánchez", label: "Pedro Sánchez" },
+      { value: "Elena Torres", label: "Elena Torres" },
+      { value: "Miguel Herrera", label: "Miguel Herrera" },
+      { value: "Laura Castillo", label: "Laura Castillo" },
+      { value: "Javier Morales", label: "Javier Morales" },
+      { value: "Camila Ríos", label: "Camila Ríos" },
+      { value: "Fernando Gómez", label: "Fernando Gómez" },
+      { value: "Gabriela Ortega", label: "Gabriela Ortega" },
+      { value: "Ricardo Domínguez", label: "Ricardo Domínguez" },
+      { value: "Valeria Mendoza", label: "Valeria Mendoza" },
+      { value: "Daniela Chávez", label: "Daniela Chávez" },
+      { value: "Héctor Vargas", label: "Héctor Vargas" },
+      { value: "Patricia Navarro", label: "Patricia Navarro" },
+      { value: "Andrés Paredes", label: "Andrés Paredes" },
+      { value: "Natalia Guzmán", label: "Natalia Guzmán" },
+      { value: "Sebastián Figueroa", label: "Sebastián Figueroa" },
+      { value: "Mariana López", label: "Mariana López" },
+    ];
+
+    return users.filter((user) =>
+      user.label.toLowerCase().includes(query.toLowerCase())
+    );
+  },
+  shouldDisplay: async () => {
+    return true; // Aquí puedes cambiar la lógica para que dependa de alguna condición
+  },
 };
 
-const Home = () => {
-  const [formData, setFormData] = useState(null); // Estado para almacenar los datos del formulario
+// Métodos para 'dynamicSelect'
+const dynamicSelectMethods = {
+  fetchOptions: async () => {
+    return [
+      { value: "perro", label: "Perro" },
+      { value: "gato", label: "Gato" },
+      { value: "elefante", label: "Elefante" },
+      { value: "tigre", label: "Tigre" },
+      { value: "león", label: "León" },
+      { value: "jirafa", label: "Jirafa" },
+      { value: "oso", label: "Oso" },
+      { value: "cocodrilo", label: "Cocodrilo" },
+      { value: "águila", label: "Águila" },
+      { value: "lobo", label: "Lobo" },
+      { value: "tortuga", label: "Tortuga" },
+      { value: "serpiente", label: "Serpiente" },
+      { value: "caballo", label: "Caballo" },
+      { value: "delfín", label: "Delfín" },
+      { value: "pingüino", label: "Pingüino" },
+      { value: "canguro", label: "Canguro" },
+      { value: "zorro", label: "Zorro" },
+      { value: "ardilla", label: "Ardilla" },
+      { value: "murciélago", label: "Murciélago" },
+      { value: "camaleón", label: "Camaleón" },
+    ];
+  },
+  shouldDisplay: async () => {
+    return true; // Cambia la condición aquí
+  },
+};
 
-  const requestMethods = {
-    userSearch: {
-      searchUser: async (query) => {
-        const users = [
-          { value: "Aaron Mendoza", label: "Aaron Mendoza" },
-          { value: "Abel Rodríguez", label: "Abel Rodríguez" },
-          { value: "Abigail Fernández", label: "Abigail Fernández" },
-          { value: "Abril Gómez", label: "Abril Gómez" },
-          { value: "Adriana Pérez", label: "Adriana Pérez" },
-          { value: "Agustín Torres", label: "Agustín Torres" },
-          { value: "Aída Ramírez", label: "Aída Ramírez" },
-          { value: "Alan Domínguez", label: "Alan Domínguez" },
-          { value: "Ana Fernández", label: "Ana Fernández" },
-          { value: "Luis Rodríguez", label: "Luis Rodríguez" },
-          { value: "Sofía Ramírez", label: "Sofía Ramírez" },
-          { value: "Pedro Sánchez", label: "Pedro Sánchez" },
-          { value: "Elena Torres", label: "Elena Torres" },
-          { value: "Miguel Herrera", label: "Miguel Herrera" },
-          { value: "Laura Castillo", label: "Laura Castillo" },
-          { value: "Javier Morales", label: "Javier Morales" },
-          { value: "Camila Ríos", label: "Camila Ríos" },
-          { value: "Fernando Gómez", label: "Fernando Gómez" },
-          { value: "Gabriela Ortega", label: "Gabriela Ortega" },
-          { value: "Ricardo Domínguez", label: "Ricardo Domínguez" },
-          { value: "Valeria Mendoza", label: "Valeria Mendoza" },
-          { value: "Daniela Chávez", label: "Daniela Chávez" },
-          { value: "Héctor Vargas", label: "Héctor Vargas" },
-          { value: "Patricia Navarro", label: "Patricia Navarro" },
-          { value: "Andrés Paredes", label: "Andrés Paredes" },
-          { value: "Natalia Guzmán", label: "Natalia Guzmán" },
-          { value: "Sebastián Figueroa", label: "Sebastián Figueroa" },
-          { value: "Mariana López", label: "Mariana López" },
-        ];
-        
-        return users.filter((user) =>
-          user.label.toLowerCase().includes(query.toLowerCase())
-        );
-      },
-      shouldDisplay: async () => {
-        return true; // Aquí puedes cambiar la lógica para que dependa de alguna condición
-      },
-    },
-    dynamicSelect: {
-      fetchOptions: async () => {
-        return [
-          { value: "perro", label: "Perro" },
-          { value: "gato", label: "Gato" },
-          { value: "elefante", label: "Elefante" },
-          { value: "tigre", label: "Tigre" },
-          { value: "león", label: "León" },
-          { value: "jirafa", label: "Jirafa" },
-          { value: "oso", label: "Oso" },
-          { value: "cocodrilo", label: "Cocodrilo" },
-          { value: "águila", label: "Águila" },
-          { value: "lobo", label: "Lobo" },
-          { value: "tortuga", label: "Tortuga" },
-          { value: "serpiente", label: "Serpiente" },
-          { value: "caballo", label: "Caballo" },
-          { value: "delfín", label: "Delfín" },
-          { value: "pingüino", label: "Pingüino" },
-          { value: "canguro", label: "Canguro" },
-          { value: "zorro", label: "Zorro" },
-          { value: "ardilla", label: "Ardilla" },
-          { value: "murciélago", label: "Murciélago" },
-          { value: "camaleón", label: "Camaleón" },
-        ];
-        
-      },
-      shouldDisplay: async () => {
-        return true; // Cambia la condición aquí
-      },
-    },
-    multipleSelect: {
-      fetchOptions: async () => {
-        return [
-          { value: "rojo", label: "Rojo" },
-          { value: "azul", label: "Azul" },
-          { value: "verde", label: "Verde" },
-          { value: "amarillo", label: "Amarillo" },
-          { value: "naranja", label: "Naranja" },
-          { value: "morado", label: "Morado" },
-          { value: "rosa", label: "Rosa" },
-          { value: "negro", label: "Negro" },
-          { value: "blanco", label: "Blanco" },
-          { value: "gris", label: "Gris" },
-          { value: "cian", label: "Cian" },
-          { value: "magenta", label: "Magenta" },
-          { value: "turquesa", label: "Turquesa" },
-          { value: "lavanda", label: "Lavanda" },
-          { value: "dorado", label: "Dorado" },
-          { value: "plateado", label: "Plateado" },
-          { value: "marrón", label: "Marrón" },
-          { value: "beige", label: "Beige" },
-          { value: "vino", label: "Vino" },
-          { value: "oliva", label: "Oliva" },
-        ];
-        
-      },
-      shouldDisplay: async () => {
-        return true; // Cambia la condición aquí
-      },
-    },
-    additionalDescription: {
-      shouldDisplay: async () => {
-        // Puedes agregar la lógica que desees aquí, por ejemplo:
-        return true; // O cambiar a false según tu condición
-      },
-    },
-  };
-  
+// Métodos para 'multipleSelect'
+const multipleSelectMethods = {
+  fetchOptions: async () => {
+    return [
+      { value: "rojo", label: "Rojo" },
+      { value: "azul", label: "Azul" },
+      { value: "verde", label: "Verde" },
+      { value: "amarillo", label: "Amarillo" },
+      { value: "naranja", label: "Naranja" },
+      { value: "morado", label: "Morado" },
+      { value: "rosa", label: "Rosa" },
+      { value: "negro", label: "Negro" },
+      { value: "blanco", label: "Blanco" },
+      { value: "gris", label: "Gris" },
+      { value: "cian", label: "Cian" },
+      { value: "magenta", label: "Magenta" },
+      { value: "turquesa", label: "Turquesa" },
+      { value: "lavanda", label: "Lavanda" },
+      { value: "dorado", label: "Dorado" },
+      { value: "plateado", label: "Plateado" },
+      { value: "marrón", label: "Marrón" },
+      { value: "beige", label: "Beige" },
+      { value: "vino", label: "Vino" },
+      { value: "oliva", label: "Oliva" },
+    ];
+  },
+  shouldDisplay: async () => {
+    return true; // Cambia la condición aquí
+  },
+};
 
-  // Función para manejar el formulario enviado
+// Métodos para 'additionalDescription'
+const additionalDescriptionMethods = {
+  shouldDisplay: async () => {
+    return true; // O cambiar a false según tu condición
+  },
+};
+
+// Objeto grande que incluye los métodos definidos afuera
+const requestMethods = {
+  userSearch: userSearchMethods,
+  dynamicSelect: dynamicSelectMethods,
+  multipleSelect: multipleSelectMethods,
+  additionalDescription: additionalDescriptionMethods,
+};
+
+
   const handleFormSubmit = (data) => {
-    setFormData(data); // Guarda los datos recibidos en el estado
+    setFormData(data);
     console.log("Formulario recibido en el padre:");
-    console.log(JSON.stringify(data, null, 2)); // Muestra los datos recibidos en el padre
+    console.log(JSON.stringify(data, null, 2));
   };
 
   return (
@@ -143,11 +137,9 @@ const Home = () => {
       <h1>¡Hola, bienvenido a mi aplicación! 👋 TICKETING</h1>
       <CustomForm 
         fieldsData={fieldsData} 
-        componentMap={componentMap} 
         requestMethods={requestMethods} 
-        onSubmit={handleFormSubmit}  // Pasa la función al formulario
+        onSubmit={handleFormSubmit} 
       />
-      
       {formData && (
         <div>
           <h2>Datos del formulario enviados:</h2>
